8814beb2326c2f399904cdd49875bcf215fa2e93
Ryan Newton <rrnewton@gmail.com>
2015-02-15 14:53:16 -0500

    Push the benchmark skeletons a bit further.  Activate one case of the haskell benchmarks

--- lockfree-test/run_benchmarks.sh
+++ lockfree-test/run_benchmarks.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Begin benchmarks for lockfree-test."
-set -x
-set -e
+set -xe
 
 EXTRAARGS=$*
 
@@ -37,33 +36,52 @@ cabal build ${executable}
 
 REPORT=report_${executable}
 
-# Remove old versions to prevent inconsistent data
-for i in 1 2 4 8 16 32; do
-    CRITREPORT=$REPORT-N$i.crit
-    rm -f $CRITREPORT
-    rm -f $CRITREPORT.html
-done
+function go() {
+    WHICHBENCH=$*
+    
+    # Remove old versions to prevent inconsistent data
+    for i in 1 2 4 8 16 32; do
+	CRITREPORT=$REPORT-N$i.crit
+	rm -f $CRITREPORT
+	rm -f $CRITREPORT.html
+    done
 
-for i in 1 2 4 8 16 32; do
-    CRITREPORT=$REPORT-N$i.crit
-    ./dist/build/$executable/$executable \
-        "new/PureBag" \
-        "new/ScalableBag" \
-        "new/AdaptiveBag" \
-        "random-50-50/PureBag" \
-        "random-50-50/ScalableBag" \
-        "random-50-50/AdaptiveBag" \
-        "hotkey/PureBag" \
-        "hotkey/ScalableBag" \
-        "hotkey/AdaptiveBag" \
-        --output=$CRITREPORT.html --raw $CRITREPORT $REGRESSES +RTS -T -s -N$i
-done
+    for i in 1 2 4 8 16 32; do
+	CRITREPORT=$REPORT-N$i.crit
+	./dist/build/$executable/$executable \
+	    --output=$CRITREPORT.html --raw $CRITREPORT $REGRESSES +RTS -T -s -N$i
+    done
 
-if [ $? = 0 ]; then
-    mkdir -p reports
-    cp *.html reports
-    cp *.crit reports
-    tar -cvf reports.tar reports/
-else
-    echo "Some benchmarks errored out! Don't expect good data."
-fi
+    if [ $? = 0 ]; then
+	mkdir -p reports
+	cp *.html reports
+	cp *.crit reports
+	tar -cvf reports.tar reports/
+    else
+	echo "Some benchmarks errored out! Don't expect good data."
+    fi
+}
+
+case $BENCHVARIANT in
+    pure)
+	echo "Running pure-in-a-box benchmarks..."
+	# FIXME: use standardized names:
+	go new/PureBag random-50-50/PureBag hotkey/PureBag
+	;;
+    scalable)
+	echo "FINISHME: run scalable benchmarks here"
+	    # "new/ScalableBag" \
+	    # "random-50-50/ScalableBag" \
+	    # "hotkey/ScalableBag" \
+	;;
+    hybrid)
+	echo "FINISHME: run hybrid benchmarks here"
+	    # "new/AdaptiveBag" \
+    	    # "random-50-50/AdaptiveBag" \
+	    # "hotkey/AdaptiveBag" \
+	;;
+    *)
+	echo "ERROR: unrecognized BENCHVARIANT: $BENCHVARIANT"
+	exit 1
+	;;
+esac
