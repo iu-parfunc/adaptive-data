1c8602f388cebbabb61c2af6451461d096c65f0a
peter-fogg <peter.p.fogg@gmail.com>
2015-02-19 14:14:35 -0500

    Add ScalableBagChaseLev.

---
+++ lockfree-test/src/Data/Concurrent/ScalableBagChaseLev.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE CPP #-}
+
+module Data.Concurrent.ScalableBagChaseLev
+       -- (
+       --   ScalableBag
+       -- , newBag
+       -- , add
+       -- , remove
+       -- , osThreadID
+       -- )
+       where
+
+import Control.Concurrent
+import Data.Atomics
+import Data.Bits
+import Data.Atomics.Vector
+import qualified Data.Atomics.Counter as C
+import Data.Concurrent.Deque.ChaseLev
+import Data.TLS.PThread
+import Data.Vector (Vector(..))
+import qualified Data.Vector as V
+import System.IO.Unsafe (unsafePerformIO)
+
+dbgPrint :: String -> IO ()
+#if 0
+dbgPrint s = putStrLn $ " [dbg] "++s
+#else
+dbgPrint _ = return ()
+{-# INLINE dbgPrint #-}
+#endif
+
+--------------------------------------------------------------------------------
+
+type ScalableBag a = Vector (ChaseLevDeque a)
+
+{-# NOINLINE osThreadID #-}
+osThreadID :: TLS Int
+osThreadID = unsafePerformIO $ mkTLS $ C.incrCounter 1 threadCounter
+
+{-# NOINLINE threadCounter #-}
+threadCounter :: C.AtomicCounter
+threadCounter = unsafePerformIO $ C.newCounter 0
+
+-- | This padding factor prevents false sharing.  Cache lines on intel
+-- are 64 bytes / 8 words.
+padFactor :: Int
+padFactor = 8
+
+-- | Divide by the padding factor
+padDiv :: Int -> Int
+padDiv x = shiftR x 3
+
+newBag :: IO (ScalableBag a)
+newBag = do
+  caps <- getNumCapabilities
+  V.replicateM (padFactor * caps) newQ
+
+add :: ScalableBag a -> a -> IO ()
+add bag x = do
+  tid <- getTLS osThreadID
+  let len = padDiv (V.length bag)
+  dbgPrint$ "tid "++show tid++"Length of bag: "++show len
+  -- We try to keep this collision-free:
+  let idx = tid `mod` len
+      idx' = padFactor * idx
+  dbgPrint$ "tid "++show tid++" going into CAS loop on index "++show idx'
+  pushL (bag V.! idx') x
+
+remove :: ScalableBag a -> IO (Maybe a)
+remove bag = do
+  tid <- getTLS osThreadID
+  let idx = tid `mod` V.length bag
+      loop vec ix start | ix >= V.length vec = loop vec 0 start
+      loop vec ix start = do
+        elt <- tryPopR (vec V.! (ix*padFactor))
+        case elt of
+          Nothing -> let ix' = ix + 1 in
+            if ix' == start || (ix' >= V.length vec && start == 0)
+            then return Nothing -- looped around once, nothing to pop
+            else loop vec ix' start -- keep going
+          jx -> return jx
+  loop bag idx idx
+
+-- remove :: ScalableBag a -> IO (Maybe a)
+-- remove bag = do
+--   tid <- getTLS osThreadID
+--   let idx = tid `mod` V.length bag
+--       retryLoop vec ix start | ix >= V.length vec = retryLoop vec 0 start
+--       retryLoop vec ix start = do
+--         tick <- unsafeReadVectorElem bag ix
+--         case peekTicket tick of
+--           [] -> let ix' = ix + 1 in
+--             if ix' == start || (ix' >= V.length vec && start == 0)
+--             then return Nothing -- looped around once, nothing to pop
+--             else retryLoop vec ix' start -- keep going
+--           _ -> do
+--             res <- casVectorLoop bag pop ix
+--             case res of
+--               Nothing -> retryLoop vec ix start -- someone else stole what we were going to pop
+--               jx -> return jx
+--       pop [] = ([], Nothing)
+--       pop (x:xs) = (xs, Just x)
+--   retryLoop bag idx idx
+
+-- {-# INLINE casVectorLoop_ #-}
+-- casVectorLoop_ :: Vector a -> (a -> a) -> Int -> IO ()
+-- casVectorLoop_ vec f ix = retryLoop =<< readVectorElem vec ix
+--   where retryLoop tick = do
+--           let !val    = peekTicket tick
+--               !newVal = f val
+--           (success, tick') <- casVectorElem vec ix tick newVal
+--           if success then return () else retryLoop tick'
+
+-- {-# INLINE casVectorLoop #-}
+-- casVectorLoop :: IOVector a -> (a -> (a, b)) -> Int -> IO b
+-- casVectorLoop vec f ix = retryLoop =<< readVectorElem vec ix
+--   where retryLoop tick = do
+--           let !val = peekTicket tick
+--               (!newVal, !ret) = f val
+--           (success, tick') <- casVectorElem vec ix tick newVal
+--           if success then return ret else retryLoop tick'
