656836a498f2a593dabf660293cda50f816b03c6
alivaramesh <alivara@indiana.edu>
2015-02-25 16:32:20 -0500

    new cost test added

--- j_benchmark/src/main/java/benchmark/ObjectCreationCost.java
+++ j_benchmark/src/main/java/benchmark/ObjectCreationCost.java
@@ -3,35 +3,46 @@ package benchmark;
 import hybrid_ds.HybridIntMap;
 import hybrid_ds.PureIntMap;
 
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class ObjectCreationCost {
 
 	int numNewObjects;
 	boolean warmUp;
+	private ArrayList<ConcurrentSkipListMap<Integer, ConcurrentSkipListMap<Integer, Integer>>> scalableArray = new ArrayList<ConcurrentSkipListMap<Integer, ConcurrentSkipListMap<Integer, Integer>>>();
+	private ArrayList<HybridIntMap<HybridIntMap<Integer>>> hybridArray = new ArrayList<HybridIntMap<HybridIntMap<Integer>>>();
+	private ArrayList<PureIntMap<PureIntMap<Integer>>> pureArray = new ArrayList<PureIntMap<PureIntMap<Integer>>>();
 
 	public ObjectCreationCost() {
 
 		System.out.println("ObjectCreationCost");
 		System.out.println("Warming UP");
-		numNewObjects = 1000000;
+		numNewObjects = 1000;
 		warmUp = true;
 		benchPure();
 		benchScalable();
 		benchHybrid();
 		warmUp = false;
 		System.out.println("Benchmarking");
-		numNewObjects = 10000000;
+		numNewObjects = 10000;
+		System.out.println(String.format("creating %d new objects",
+				numNewObjects));
 		benchPure();
 		benchScalable();
 		benchHybrid();
+
+		scalableArray = new ArrayList<ConcurrentSkipListMap<Integer, ConcurrentSkipListMap<Integer, Integer>>>();
+		hybridArray = new ArrayList<HybridIntMap<HybridIntMap<Integer>>>();
+		pureArray = new ArrayList<PureIntMap<PureIntMap<Integer>>>();
 	}
 
 	private void benchScalable() {
 
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < numNewObjects; i++) {
-			new ConcurrentSkipListMap<Integer, ConcurrentSkipListMap<Integer, Integer>>();
+			scalableArray
+					.add(new ConcurrentSkipListMap<Integer, ConcurrentSkipListMap<Integer, Integer>>());
 		}
 		long endTime = System.currentTimeMillis();
 		if (!warmUp) {
@@ -43,7 +54,7 @@ public class ObjectCreationCost {
 
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < numNewObjects; i++) {
-			new HybridIntMap<HybridIntMap<Integer>>(1);
+			hybridArray.add(new HybridIntMap<HybridIntMap<Integer>>(1));
 		}
 		long endTime = System.currentTimeMillis();
 		if (!warmUp) {
@@ -55,7 +66,7 @@ public class ObjectCreationCost {
 
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < numNewObjects; i++) {
-			new PureIntMap<PureIntMap<Integer>>();
+			pureArray.add(new PureIntMap<PureIntMap<Integer>>());
 		}
 		long endTime = System.currentTimeMillis();
 		if (!warmUp) {
