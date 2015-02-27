407512795f33258dc8589076f80a4f717031df26
alivaramesh <alivara@indiana.edu>
2015-02-27 03:51:13 -0500

    benchmark script updated

--- j_benchmark/run_benchmarks.sh
+++ j_benchmark/run_benchmarks.sh
@@ -3,7 +3,7 @@
 set -xe
 
 TABLENAME=AdaptivelyScalable
-CSVUPLOAD=hsbencher-fusion-upload-csv-0.3.15
+CSVUPLOAD=hsbencher-fusion-upload-csv-0.3.12
 
 MAVEN=mvn
 JAVA_EXEC=java
@@ -34,7 +34,7 @@ case $BENCHVARIANT in
     #do
 		#	$JAVA_RUN -cp  target/j_benchmark-0.0.1.jar benchmark.SimpleInsertionBenchmark $BENCHVARIANT $(($NUM_INSERTS * $i)) $BENCHMARK_ROUNDS $MAX_NUM_THREADS $TIME_MILLI_SECONDS 
 	#done
-	for HOT_RATIO in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
+	for HOT_RATIO in 1.0;
 	do
 		$JAVA_RUN -cp  target/j_benchmark-0.0.1.jar benchmark.RandomHotColdkeyBecnhmark $BENCHVARIANT $NUM_INSERTS $NUM_HOTKEYS $BENCHMARK_ROUNDS $MAX_NUM_THREADS $HOT_RATIO $CAS_TRIES $TIME_MILLI_SECONDS
 	done  
