50d90d1a524df8c5799aa5a2b1575196c4aed9ee
Vikraman Choudhury <git@vikraman.org>
2016-03-01 18:18:34 -0500

    Abstract out the spinlock

--- adaptive-hashmap/src/Data/Concurrent/IORef.hs
+++ adaptive-hashmap/src/Data/Concurrent/IORef.hs
@@ -1,26 +1,31 @@
 {-# LANGUAGE BangPatterns #-}
-module Data.Concurrent.IORef
-       (
-         IORef,
-         IOVal,
-         newIORef,
-         readIORef,
-         casIORef,
-         readForCAS,
-         peekTicket,
-         freezeIORef,
-         CASIORefException(CASIORefException),
-         TransitionException(TransitionException)
-       )
-       where
+
+module Data.Concurrent.IORef (
+    IORef,
+    IOVal,
+    newIORef,
+    readIORef,
+    casIORef,
+    readForCAS,
+    peekTicket,
+    freezeIORef,
+    spinlock,
+    CASIORefException(CASIORefException),
+    TransitionException(TransitionException),
+    ) where
 
 import           Control.Exception
+import           Control.Monad
 import qualified Data.Atomics      as A
 import qualified Data.IORef        as IR
 import           Data.Typeable
 
-newtype IORef t = IORef (IR.IORef (IOVal t)) deriving (Eq, Typeable)
-data IOVal t = Val !t | Frozen !t deriving (Show, Typeable)
+newtype IORef t = IORef (IR.IORef (IOVal t))
+  deriving (Eq, Typeable)
+
+data IOVal t = Val !t
+             | Frozen !t
+  deriving (Show, Typeable)
 
 data CASIORefException = CASIORefException deriving (Show, Typeable)
 instance Exception CASIORefException
@@ -39,7 +44,7 @@ readIORef :: IORef a -> IO a
 readIORef (IORef !ref) = do
   !v <- IR.readIORef ref
   case v of
-    Val    t -> return t
+    Val t    -> return t
     Frozen t -> return t
 
 {-# INLINABLE readForCAS #-}
@@ -50,19 +55,26 @@ readForCAS (IORef !ref) = A.readForCAS ref
 peekTicket :: A.Ticket (IOVal a) -> a
 peekTicket !tik =
   case A.peekTicket tik of
-  Val t -> t
-  Frozen t -> t
+    Val t    -> t
+    Frozen t -> t
 
 {-# INLINABLE casIORef #-}
 casIORef :: IORef a -> A.Ticket (IOVal a) -> a -> IO(Bool, A.Ticket (IOVal a))
-casIORef (IORef !ref) !tik !a = do
+casIORef (IORef !ref) !tik !a =
   case A.peekTicket tik of
     Frozen _ -> throwIO CASIORefException
-    Val    _ -> A.casIORef ref tik $ Val a
+    Val _    -> A.casIORef ref tik $ Val a
 
 {-# INLINABLE freezeIORef #-}
 freezeIORef :: IORef a -> A.Ticket (IOVal a) -> IO(Bool, A.Ticket (IOVal a))
-freezeIORef (IORef !ref) !tik = do
+freezeIORef (IORef !ref) !tik =
   case A.peekTicket tik of
     Frozen _ -> return (True, tik)
-    Val    a -> A.casIORef ref tik $ Frozen a
+    Val a    -> A.casIORef ref tik $ Frozen a
+
+{-# INLINE spinlock #-}
+spinlock :: (A.Ticket a -> IO (Bool, A.Ticket a))
+         -> A.Ticket a -> IO ()
+spinlock !f !tik = do
+  (!success, !tik') <- f tik
+  unless success $ spinlock f tik'
