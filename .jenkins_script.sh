16a6a5b4781915403e0edc5705795341bc3bc8ac
Ryan Newton <rrnewton@gmail.com>
2016-03-05 09:09:11 -0500

    Jenkins script wasnt building adaptive-hashmap... fixed

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+set -xe
 
 which -a stack
 stack --version
+
+TOP=`pwd`
+
+# Building our current/recent version [2016.03.05]:
+cd "$TOP/adaptive-hashmap/"
+stack --install-ghc --no-system-ghc build
+
+# Building the top-level repo builds a bunch of the old code:
+cd "$TOP/"
 stack --install-ghc --no-system-ghc build
