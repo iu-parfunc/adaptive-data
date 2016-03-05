de6f371decddfbd15d016f9fe5d0c9b18057a3fb
Ryan Newton <rrnewton@gmail.com>
2016-03-05 09:17:22 -0500

    Tweak jenkins/stack.yaml to setup environment

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -9,8 +9,10 @@ TOP=`pwd`
 
 # Building our current/recent version [2016.03.05]:
 cd "$TOP/adaptive-hashmap/"
+source setup_env.sh
 stack --install-ghc --no-system-ghc build
 
-# Building the top-level repo builds a bunch of the old code:
+# Building the top-level repo builds a bunch of the old code.
+# This uses an LTS build so it should ignore the GHC on path.
 cd "$TOP/"
 stack --install-ghc --no-system-ghc build
--- adaptive-hashmap/setup_env.sh
+++ adaptive-hashmap/setup_env.sh
@@ -1,3 +1,4 @@
 # source me
 
+# Bring the GHC with Data.Compact into path:
 export PATH=$HOME/opt/ghc-mutable-cnf-0.2/bin/:$PATH
