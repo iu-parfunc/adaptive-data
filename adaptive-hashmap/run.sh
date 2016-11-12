5ed0dfc263a7919dec7f58162f9f55a3bde2886c
Chao-Hong Chen <dreamlinuxer@gmail.com>
2016-11-12 00:04:34 -0500

    script

--- adaptive-hashmap/run.sh
+++ adaptive-hashmap/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-stack bench adaptive-hashmap:pldi-bench --benchmark-arguments "--bench ctrie --dir data --seed `date +'%s'` --ops 10000 --unit 100 +RTS -N16"
-stack bench adaptive-hashmap:pldi-bench --benchmark-arguments "--bench gz --dir data --seed `date +'%s'` --ops 10000 --unit 100 +RTS -N16"
+SEED=`date +'%s'`
+stack bench adaptive-hashmap:pldi-bench --benchmark-arguments "--bench ctrie    --dir data --seed ${SEED} --ops 100000 --unit 1000 --ndb 10 +RTS -N16"
+stack bench adaptive-hashmap:pldi-bench --benchmark-arguments "--bench adaptive --dir data --seed ${SEED} --ops 100000 --unit 1000 --ndb 10 +RTS -N16"
